1e64c661cfbcdce0ade268420f7940c0f9412e00
kwkhaw <kwkhaw@gmail.com>
2016-08-13 22:09:34 +0800

    Add webpack configuration for devServer

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,12 @@ module.exports = {
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
-  }
+  },
+
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  } 
 }
