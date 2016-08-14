1a6a5baeaed047f92445e536781a897487cdd3dd
kwkhaw <kwkhaw@gmail.com>
2016-08-14 19:28:52 +0800

    Add Optimization Plugins (OccurenceOrderPlugin, UglifyJsPlugin and ExtractTextPlugin) for production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: __dirname + "/app/main.js",
@@ -20,7 +21,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style!css?modules!postcss'
+        loader: ExtractTextPlugin.extract('style', 'css?modules!postcss')
       }
     ]
   },
@@ -31,5 +32,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + "/app/index.tmpl.html"
     }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin("style.css")
   ],
 }
